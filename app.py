e5b54a954833769ba54afaaa49111524cd49cbab
RisparmiainBolletta <risparmiainbolletta.com@gmail.com>
2025-03-31 21:40:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 import tempfile
+import json
 
 app = Flask(__name__, static_folder="frontend")
 app.secret_key = 'supersegreto123'
